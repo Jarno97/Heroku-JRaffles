1a1636b8bd7e2073b18f3c2ff3d526cc6e35c55c
Jarno97 <jarno_swinkels@live.nl>
2023-04-05 17:06:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ app.get('/version', (req, res) => {
 
 });
 
+app.get('/entries/xJ4doru9Dkfwda9', (req, res) => {
+    var Entries;
+    MyModel.find({}, (err, documents) => {
+        if (err) {
+            console.error(err);
+        } else {
+            Entries = documents;
+        }
+    });
+    res.send(Entries);
+})
+
 app.post('/success', (req, res) => {
     console.log(req.body)
     var data = req.body
